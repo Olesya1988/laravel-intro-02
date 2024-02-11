f4edfc13917d97126b945e883dc75517107395fc
Olesya Vetoshkina <our_post-office@mail.ru>
2024-02-11 03:53:40 +0300

    bug fixes from 02/11/2024 commit

--- database/migrations/2024_02_09_023729_create_students_table.php
+++ database/migrations/2024_02_09_023729_create_students_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('group_id');
+            $table->foreignId('group_id')->constrained();
             $table->string('surname');
             $table->string('name');
             $table->timestamp('created_at')->nullable();
